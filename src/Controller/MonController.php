0c13c32068010adc29c89ab3b84dae5471740494
RomainL83 <locquet.romain83@gmail.com>
2024-01-08 16:18:17 +0100

    ajout du controllercontact

--- src/Controller/MonController.php
+++ src/Controller/MonController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 class MonController extends AbstractController
 {
     #[Route('/ma-premiere-page', name: 'ma_premiere_page')]
@@ -13,4 +14,21 @@ class MonController extends AbstractController
     {
         return $this->render('mon_template.html.twig');
     }
+}
+class MyController extends AbstractController
+{
+public function sendEmailAction(Request $request, MailerInterface $mailer): Response
+{
+
+   $email = (new Email())
+       ->from('didierdeschamps@example.com')
+       ->to('zinedine@example.com')
+       ->subject('Coupe du monde')
+       ->text('vous êtes invité à la prochaine coupe du monde');
+
+   $mailer->send($email);
+
+
+return $this->redirectToRoute('homepage');
+}
 }
